2231ba9905fa5d9afd172298fda08696d7bf6c34
neboskrebnut <ivanov.arseniy@gmail.com>
2018-04-05 16:01:52 -0400

    first

--- Assignment5/Assignment5.cpp
+++ Assignment5/Assignment5.cpp
@@ -4,8 +4,34 @@
 #include "stdafx.h"
 
 
-int main()
+using namespace std;
+int main(int argc, char * argv[])
 {
-    return 0;
+	cout << "number of arguments: " << argc << endl;
+	cout << "command line arguments: ";
+	for (int i = 0; i < argc; i++)
+	{
+		cout << argv[i];
+		cout <<endl;
+	}
+	cout << endl;
+
+	//source file opening
+	ifstream sFile(argv[1]);
+	if (!sFile.is_open())
+		cout << "\nFIle is unavailable";
+
+	//target file openning
+	ofstream tFile(argv[2]);
+	if (!tFile.is_open())
+		cout << "\nFIle is unavailable";
+
+	tFile << 
+
+	sFile.close();
+	tFile.close();
+
+	cin.get();
+	return 0;
 }
 
