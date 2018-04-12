026464d29ad4e9a05cff415a2cceca55eb63f612
Broflowsky <v.puyfourcat@gmail.com>
2018-04-12 17:41:17 -0400

    all good

--- Assignment5/Assignment5.cpp
+++ Assignment5/Assignment5.cpp
@@ -1,37 +1,71 @@
 // Assignment5.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "json.hpp"
 
 using namespace std;
+using namespace nlohmann;
+
+//argv[0] is the exe file
+//argv[1] is -s (don't know the point of that)
+//argv[2] is the field by which we want to sort //
+//argv[3] is the input file
+//argv[4] is the output file
+
+// very useful link
+
+//https://codereview.stackexchange.com/questions/167121/reading-and-storing-nested-data
+
+
 int main(int argc, char * argv[])
 {
-	cout << "number of arguments: " << argc << endl;
-	cout << "command line arguments: ";
-	for (int i = 0; i < argc; i++)
-	{
-		cout << argv[i];
-		cout <<endl;
+
+	cout<<"Argument entered: "<<argc<<'\n';
+
+	try{
+		if(!argc)
+			throw "No argument entered";
+
+	}catch(string& str){
+		cerr<<str;
+		exit(1);
 	}
-	cout << endl;
 
-	//source file opening
-	ifstream sFile(argv[1]);
-	if (!sFile.is_open())
-		cout << "\nFIle is unavailable";
+	for(int i=0; i<argc;++i)
+		cout<<argv[i]<<'\n';
 
-	//target file openning
-	ofstream tFile(argv[2]);
-	if (!tFile.is_open())
-		cout << "\nFIle is unavailable";
+	ifstream iFile(argv[3]);
+	ofstream oFile(argv[4]);
 
-	tFile << 
+	try{
+		if (!iFile.is_open())
+			throw "\niFile is unavailable";
+		if (!oFile.is_open())
+			throw "\noFIle is unavailable";
+	}
+	catch(string& str){
+		cerr<<str;
+		exit(1);
+	}
 
-	sFile.close();
-	tFile.close();
+	json j;
 
-	cin.get();
+	iFile >> j;
+	oFile << setw(3) << j << '\n';
+
+
+
+	iFile.close();
+	oFile.close();
+
+
+
+	//cin.get();
 	return 0;
 }
 
