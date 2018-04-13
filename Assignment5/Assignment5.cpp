54d0d823b2a5d7392463f8302ab3eeabc8f29a98
neboskrebnut <ivanov.arseniy@gmail.com>
2018-04-13 13:41:12 -0400

    1

--- Assignment5/Assignment5.cpp
+++ Assignment5/Assignment5.cpp
@@ -1,7 +1,8 @@
 // Assignment5.cpp : Defines the entry point for the console application.
 //
 
-//#include "stdafx.h"
+#include "stdafx.h"
+
 
 #include <iostream>
 #include <iomanip>
@@ -14,13 +15,12 @@ using namespace std;
 using namespace nlohmann;
 
 //argv[0] is the exe file
-//argv[1] is -s (don't know the point of that)
+//argv[1] is -s 
 //argv[2] is the field by which we want to sort //
 //argv[3] is the input file
 //argv[4] is the output file
 
 // very useful link
-
 //https://codereview.stackexchange.com/questions/167121/reading-and-storing-nested-data
 
 
@@ -69,12 +69,13 @@ int main(int argc, char * argv[])
 
 		for(size_t k = 0; !j["items"]["item"][i]["batters"]["batter"][k].is_null();++k)
 		{//should iterate through batter
+			for(size_t l = 0; !j["items"]["item"][i]["toppings"][l].is_null();++l){
+						;
+					}
 
-			;/*write id , type , name */
+			;//write id , type , name
 		}
-		for(size_t l = 0; !j["items"]["item"][i]["toppings"][l].is_null();++l){
-			;
-		}
+		
 	}
 
 
@@ -85,12 +86,10 @@ int main(int argc, char * argv[])
 
 
 
-	iFile.close();
-	oFile.close();
+	//iFile.close();
+	//oFile.close();
 
-
-
-	//cin.get();
+	cin.get();
 	return 0;
 }
 
